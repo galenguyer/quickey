02dfb27fdbf47f7b0538716f23ad67aa27b15228
Galen Guyer <galen@galenguyer.com>
2023-05-02 10:25:30 -0400

    Add verbose CLI flag

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ struct Cli {
 
     #[arg(short, long, default_value_t = 32)]
     bytes: usize,
+
+    #[arg(short, long)]
+    verbose: bool,
 }
 
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -20,8 +23,22 @@ enum Algorithm {
     Hex,
 }
 
+impl core::fmt::Display for Algorithm {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Algorithm::Base58 => write!(f, "base58"),
+            Algorithm::Base64 => write!(f, "base64"),
+            Algorithm::Hex => write!(f, "hex"),
+        }
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
+
+    if cli.verbose {
+        println!("generating {} bytes using {}", cli.bytes, cli.algorithm);
+    }
 
     match cli.algorithm {
         Algorithm::Base58 => println!("{}", algorithms::base58(cli.bytes)),
