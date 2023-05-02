24dc0c3136018b5b7d10ba09e3c650e1e2259ba7
Galen Guyer <galen@galenguyer.com>
2023-05-02 10:05:26 -0400

    Add Clap for CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use clap::{Parser, ValueEnum};
+
 mod base58;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(value_enum, default_value_t = Algorithm::Base58)]
+    algorithm: Algorithm,
+
+    #[arg(short, long, default_value_t = 32)]
+    bytes: usize,
+}
+
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Algorithm {
+    #[default]
+    Base58,
+}
+
 fn main() {
-    println!("{}", base58::base58(32))
+    let cli = Cli::parse();
+
+    match cli.algorithm {
+        Algorithm::Base58 => println!("{}", base58::base58(cli.bytes)),
+    }
 }
